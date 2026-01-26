30d508db80ca93d855866ffa80abd19c6edc1042
HigorAlves9770 <higor.castro@ufms.br>
2026-01-26 17:36:26 -0300

    adicionando exercicio de JPA

--- SpringBoot/api-produtos/src/main/java/com/br/api_produtos/ApiProdutosApplication.java
+++ SpringBoot/api-produtos/src/main/java/com/br/api_produtos/ApiProdutosApplication.java
@@ -1,15 +1,12 @@
 package com.br.api_produtos;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 
 @SpringBootApplication
 public class ApiProdutosApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiProdutosApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ApiProdutosApplication.class, args);
+    }
 }
--- SpringBoot/api-produtos/src/main/java/com/br/api_produtos/Model/Product.java
+++ SpringBoot/api-produtos/src/main/java/com/br/api_produtos/Model/Product.java
@@ -1,19 +1,30 @@
 package com.br.api_produtos.Model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "products")
 public class Product {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
     private String name;
+
+    @Column(nullable = false)
     private double price;
 
     public Product() {}
 
     public Long getId() {
         return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
     }
 
     public String getName() {
@@ -32,4 +43,3 @@ public class Product {
         this.price = price;
     }
 }
-
