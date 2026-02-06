704599c862b8da3b785a916d769a409dcf9d7988
HigorAlves9770 <higor.castro@ufms.br>
2026-02-06 00:37:53 -0300

    atualizando

--- SpringBoot/api-produtos/src/main/java/com/br/api_produtos/ApiProdutosApplication.java
+++ SpringBoot/api-produtos/src/main/java/com/br/api_produtos/ApiProdutosApplication.java
@@ -3,8 +3,8 @@ package com.br.api_produtos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// Inicializa a aplicação - classe principal responsável pela inicialização do código
-// Starts the application - main class responsible for initializing the application
+// Inicializa a aplicação - classe principal responsável pela inicialização do código.
+// Starts the application - main class responsible for initializing the application.
 @SpringBootApplication
 public class ApiProdutosApplication {
 
